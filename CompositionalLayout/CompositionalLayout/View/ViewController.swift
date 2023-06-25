dfdea78b3fe760369f49cc71656fe4b0f137b303
oguuk <ogw135@gmail.com>
2023-06-25 23:28:23 +0900

    feat: UICollectionViewCompositionalLayout 생성 메서드

--- CompositionalLayout/CompositionalLayout/View/ViewController.swift
+++ CompositionalLayout/CompositionalLayout/View/ViewController.swift
@@ -39,3 +39,106 @@ class ViewController: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+    
+    private func configureLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionNum, env in
+            
+            switch sectionNum {
+            case 0:
+                let item: NSCollectionLayoutItem = .init(
+                    layoutSize:
+                            .init(
+                                widthDimension: .fractionalWidth(1),
+                                heightDimension: .fractionalHeight(1)
+                            )
+                )
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .absolute(200)
+                    ),
+                    subitems: [item]
+                )
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPaging
+                return section
+            case 1:
+                let item: NSCollectionLayoutItem = .init(
+                    layoutSize:
+                            .init(
+                                widthDimension: .fractionalWidth(0.25),
+                                heightDimension: .absolute(150)
+                            )
+                )
+                item.contentInsets.bottom = 16
+                item.contentInsets.trailing = 16
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .estimated(500)
+                    ),
+                    subitems: [item]
+                )
+                let section = NSCollectionLayoutSection(group: group)
+                section.contentInsets.leading = 16
+                section.boundarySupplementaryItems = [
+                    .init(
+                        layoutSize: .init(
+                            widthDimension: .fractionalWidth(1),
+                            heightDimension: .absolute(50)
+                        ),
+                        elementKind: "secondHeaderID",
+                        alignment: .topLeading
+                    )
+                ]
+                return section
+            case 2:
+                let item: NSCollectionLayoutItem = .init(
+                    layoutSize:
+                            .init(
+                                widthDimension: .fractionalWidth(1),
+                                heightDimension: .absolute(80)
+                            )
+                )
+                item.contentInsets.trailing = 32
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                      heightDimension: .absolute(200)),
+                    subitems: [item]
+                )
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPaging
+                section.contentInsets.leading = 16
+                section.contentInsets.top = 16
+                return section
+            default:
+                let item: NSCollectionLayoutItem = .init(
+                    layoutSize:
+                            .init(
+                                widthDimension: .fractionalWidth(1),
+                                heightDimension: .fractionalHeight(1)
+                            )
+                )
+                item.contentInsets.trailing = 16
+                item.contentInsets.top = 16
+                let group = NSCollectionLayoutGroup.vertical(
+                    layoutSize: .init(widthDimension: .fractionalWidth(0.9),
+                                      heightDimension: .estimated(300)),
+                    repeatingSubitem: item,
+                    count: 5
+                )
+                let section = NSCollectionLayoutSection(group: group)
+                section.boundarySupplementaryItems = [
+                    .init(layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                            heightDimension: .absolute(50)),
+                          elementKind: "thirdHeaderID",
+                          alignment: .topLeading
+                         )
+                ]
+                section.contentInsets.leading = 16
+                section.orthogonalScrollingBehavior = .groupPaging
+                return section
+            }
+        }
+    }
+}
