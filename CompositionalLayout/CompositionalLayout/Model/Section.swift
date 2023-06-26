111bc1c042ad372a6940c96704b1c05c9f692887
oguuk <ogw135@gmail.com>
2023-06-26 17:45:45 +0900

    feat: DiffableDataSource의 SEctionIdentifierType

---
+++ CompositionalLayout/CompositionalLayout/Model/Section.swift
@@ -0,0 +1,12 @@
+//
+//  Section.swift
+//  CompositionalLayout
+//
+//  Created by 오국원 on 2023/06/26.
+//
+
+import Foundation
+
+enum Section: CaseIterable {
+    case section1, section2, section3, section4
+}
